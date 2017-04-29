b89bcf37579d0b8349dd5f16fbdcc477d99256ee
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2017-04-30 00:24:10 +0100

    Blah

--- comm.go
+++ comm.go
@@ -19,7 +19,7 @@ type JobMessage struct{
 	Service string `json:"service"`
 }
 
-var locs = map[string]string{"sum":"sum/job","prod":"prod/job"}
+var locs = map[string]string{"sum":"sum:8000/job","prod":"prod:8000/job"}
 
 func (j *JobMessage) Recieve(public bool, service ...func(data []float64, id int)) func(c echo.Context) error{
 	return func (c echo.Context) error{
