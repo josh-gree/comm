0a5f4c5eecccfa98d01c1d6fd6edd37fe6663dd0
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2017-04-30 15:39:16 +0100

    blahhh1

--- comm.go
+++ comm.go
@@ -1,55 +1,57 @@
 package comm
-// A comment
+
+// A comment BOBBBY !!!
 import (
-	"fmt"
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+
 	"github.com/labstack/echo"
 )
 
-type Message interface{
+type Message interface {
 	Recieve()
 	Send()
 }
 
-type JobMessage struct{
-	Id int `json:"id"`
-	Data []float64 `json:"data"`
-	Service string `json:"service"`
+type JobMessage struct {
+	Id      int       `json:"id"`
+	Data    []float64 `json:"data"`
+	Service string    `json:"service"`
 }
 
-var locs = map[string]string{"sum":"sum:8000/job","prod":"prod:8000/job"}
+var locs = map[string]string{"sum": "sum:8000/job", "prod": "prod:8000/job"}
 
-func (j *JobMessage) Recieve(public bool, service ...func(data []float64, id int)) func(c echo.Context) error{
-	return func (c echo.Context) error{
+func (j *JobMessage) Recieve(public bool, service ...func(data []float64, id int)) func(c echo.Context) error {
+	return func(c echo.Context) error {
 		err := c.Bind(&j)
 		if err != nil {
 			fmt.Println(err)
 			return err
 		}
-		fmt.Printf("%#v\n",j)
+		fmt.Printf("%#v\n", j)
 		if public {
 			// Send Job to service
 			j.Send(locs[j.Service])
 		} else {
 			// Do Job
 			// Send result to public
-			go service[0](j.Data,j.Id)
+			go service[0](j.Data, j.Id)
 		}
 
 		return nil
 	}
 }
 
-func (j *JobMessage) Send(dest string) error{
+func (j *JobMessage) Send(dest string) error {
 	data, err := json.Marshal(j)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 
-	_, err = http.Post(fmt.Sprintf("http://%s",dest),"application/json",bytes.NewBuffer(data))
+	_, err = http.Post(fmt.Sprintf("http://%s", dest), "application/json", bytes.NewBuffer(data))
 	if err != nil {
 		fmt.Println(err)
 		return err
@@ -57,13 +59,13 @@ func (j *JobMessage) Send(dest string) error{
 	return nil
 }
 
-type ResMessage struct{
-	Id int `json:"id"`
+type ResMessage struct {
+	Id     int     `json:"id"`
 	Result float64 `json:"result"`
 }
 
-func (r *ResMessage) Recieve(public bool) func(c echo.Context) error{
-	return func (c echo.Context) error{
+func (r *ResMessage) Recieve(public bool) func(c echo.Context) error {
+	return func(c echo.Context) error {
 		err := c.Bind(&r)
 		if err != nil {
 			fmt.Println(err)
@@ -72,7 +74,7 @@ func (r *ResMessage) Recieve(public bool) func(c echo.Context) error{
 
 		if public {
 			// Log result to stdout
-			fmt.Printf("%#v\n",r)
+			fmt.Printf("%#v\n", r)
 		} else {
 			// This will never happen (services do not recieve results)
 		}
@@ -81,14 +83,14 @@ func (r *ResMessage) Recieve(public bool) func(c echo.Context) error{
 	}
 }
 
-func (r *ResMessage) Send(dest string) error{
+func (r *ResMessage) Send(dest string) error {
 	data, err := json.Marshal(r)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 
-	_, err = http.Post(fmt.Sprintf("http://%s",dest),"application/json",bytes.NewBuffer(data))
+	_, err = http.Post(fmt.Sprintf("http://%s", dest), "application/json", bytes.NewBuffer(data))
 	if err != nil {
 		fmt.Println(err)
 		return err
